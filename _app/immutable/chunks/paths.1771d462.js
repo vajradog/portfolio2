66a80de48d3ac4bcc81d467dfe89f1043e89b44b
Thupten N Chakrishar <vajradog@users.noreply.github.com>
2023-10-11 18:21:05 -0400

    about

---
+++ _app/immutable/chunks/paths.1771d462.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1bg1wjg)==null?void 0:s.base)??"/portfolio2";var t;const e=((t=globalThis.__sveltekit_1bg1wjg)==null?void 0:t.assets)??a;export{e as a,a as b};
